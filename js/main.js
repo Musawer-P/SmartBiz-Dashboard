70e5eb0156fe9ea64857803daf741e0874cf7b46
Musawer-P <m.musawer.p@gmail.com>
2026-03-11 19:47:46 +0430

    putting codes in its files

--- js/main.js
+++ js/main.js
@@ -132,6 +132,7 @@ new Chart(barCtx, {
 
 
 
+
    const openDivVendor = document.querySelector(".openModal-vendor");
     const modalVendor = document.getElementById("modalOverlay-vendor");
     const xBtnVendor = document.getElementById("x-vendor");
